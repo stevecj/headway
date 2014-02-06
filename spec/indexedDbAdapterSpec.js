dcd962515de232ad703cb044eb4088b5ae4cde66
Steve Jorgensen <stevej@stevej.name>
2014-02-06 08:49:27 -0800

    First spec example and source file

---
+++ spec/indexedDbAdapterSpec.js
@@ -0,0 +1,11 @@
+describe( 'headway', function () {
+  "use strict";
+  var root, headway;
+  root = window;
+  headway = root.headway;
+
+  it( "is an object", function () {
+    expect( headway ).toBeTruthy();
+  });
+
+});
