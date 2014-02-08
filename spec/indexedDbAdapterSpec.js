030ea7996fb72122a74c09d76c52322831ee9ed7
Steve Jorgensen <stevej@stevej.name>
2014-02-07 18:19:42 -0800

    Create/open indexedDB connection using promise.

--- spec/indexedDbAdapterSpec.js
+++ spec/indexedDbAdapterSpec.js
@@ -1,11 +1,47 @@
-describe( 'headway', function () {
+describe( 'headway.indexedDbAdapter', function () {
   "use strict";
-  var root, headway;
+  var root, indexedDbAdapter;
   root = window;
-  headway = root.headway;
+  indexedDbAdapter = root.headway.indexedDbAdapter;
 
-  it( "is an object", function () {
-    expect( headway ).toBeTruthy();
+  describe( ".core", function() {
+    var core = indexedDbAdapter.core;
+
+    describe( ".asyncGetConnection()", function () {
+      var DB_NAME, DB_TARGET_VERSION;
+      DB_NAME = 'test';
+      DB_TARGET_VERSION = 11;
+
+      beforeEach( function () {
+        indexedDB.deleteDatabase( DB_NAME );
+      });
+
+      afterEach( function () {
+        if( this.db ) { this.db.close(); }
+        indexedDB.deleteDatabase( DB_NAME );
+      });
+
+      describe( "when the database does not exist", function () {
+
+        it( "is fulfilled with a connection to the database", function( done ) {
+          var me, asyncConnection;
+          me = this;
+          asyncConnection = core.asyncGetConnection( DB_NAME, DB_TARGET_VERSION );
+          asyncConnection.then(
+            function ( db ) {
+              me.db = db;
+              expect( db.name ).toEqual( DB_NAME );
+              expect( db.version ).toEqual( DB_TARGET_VERSION );
+            },
+            function ( e ) {
+              expect( e ).toEqual( 'no error result' );
+            }
+          ).fail( function (e) {
+            expect( e ).toEqual( 'no error result' );
+          });
+          asyncConnection.then( done, done );
+        });
+      });
+    });
   });
-
 });
--- src/script/indexedDbAdapter.js
+++ src/script/indexedDbAdapter.js
@@ -1,3 +1,21 @@
 var headway = headway || {};
 
+headway.indexedDbAdapter = headway.indexedDbAdapter || {};
 
+headway.indexedDbAdapter.core = (function ( module ) {
+
+  module.asyncGetConnection = function getConnectionable( dbName, targetVersion ) {
+    var bootstrap = ayepromise.defer();
+    bootstrap.resolve();
+
+    return bootstrap.promise.then( function () {
+      var request = indexedDB.open( dbName, targetVersion );
+      var defer = ayepromise.defer();
+      request.onsuccess = function () { defer.resolve( request.result ); };
+      request.onerror   = function () { defer.reject( request.error ); };
+      return defer.promise;
+    });
+  };
+
+  return module;
+})( headway.indexedDbAdapter.core || {} );
